c5302e9937ceb7470995aadd2741db46264f13e1
lu.haiqi <7lx-m6d11zbnf@dingtalk.com>
2020-08-18 01:02:59 +0800

    添加项目核心

--- product-core/src/main/java/org/lhq/gp/product/ProductApplication.java
+++ product-core/src/main/java/org/lhq/gp/product/ProductApplication.java
@@ -2,8 +2,13 @@ package org.lhq.gp.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author Wallace
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductApplication {
 
   public static void main(String[] args) {
