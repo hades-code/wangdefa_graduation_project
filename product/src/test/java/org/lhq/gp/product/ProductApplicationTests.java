db2ec6e8cb3b289db98f47cf08a52f8ec3225bce
lu.haiqi <7lx-m6d11zbnf@dingtalk.com>
2020-08-17 21:51:00 +0800

    项目初始化

---
+++ product/src/test/java/org/lhq/gp/product/ProductApplicationTests.java
@@ -0,0 +1,11 @@
+package org.lhq.gp.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
