decc4e6502ca22ac3440dd88b5e76bcd609b4b64
wang <lhqblue@outlook.com>
2020-08-19 21:53:37 +0800

    添加消费者，openfei

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wangdefa_graduation_project
-我的毕业设计
+王德发的毕业设计
 ## 整体设计架构
 
 ### springcloud Alibaba
---
+++ product-consumer/src/main/java/org/lhq/gp/product/ConsumerApplication.java
@@ -0,0 +1,17 @@
+package org.lhq.gp.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Wallace
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerApplication.class, args);
+  }
+}
