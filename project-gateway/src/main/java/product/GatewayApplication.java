5b6389187719494c8248c5e2ddfc863faae96a3e
wang <lhqblue@outlook.com>
2020-08-20 00:04:44 +0800

    增加gateway

--- project-gateway/src/main/java/product/GatewayApplication.java
+++ project-gateway/src/main/java/product/GatewayApplication.java
@@ -1,6 +1,5 @@
-package org.lhq.gp.product;
+package product;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,9 +9,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ConsumerApplication {
+public class GatewayApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ConsumerApplication.class, args);
+    SpringApplication.run(GatewayApplication.class, args);
   }
 }
