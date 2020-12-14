2050928c10011f3a8bb958d78b0927a44c4f3ca1
hades <lhqblue@outlook.com>
2020-12-15 02:04:51 +0800

    修复gateway跨域问题，删除冗余文件

--- gateway-service/src/main/java/org/lhq/config/CorsWebFiler.java
+++ gateway-service/src/main/java/org/lhq/config/CorsWebFiler.java
@@ -13,11 +13,16 @@ public class CorsWebFiler {
 	public CorsWebFilter getCorsFilter(){
 		CorsConfiguration config = new CorsConfiguration();
 
-		config.setAllowCredentials(true); // 允许cookies跨域
-		config.setMaxAge(1800L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
-		config.addAllowedOrigin("*");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
-		config.addAllowedHeader("*");// #允许访问的头信息,*表示全部
-		config.addAllowedMethod("OPTIONS");// 允许提交请求的方法类型，*表示全部允许
+		config.setAllowCredentials(true);
+		// 允许cookies跨域
+		config.setMaxAge(1800L);
+		// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+		config.addAllowedOrigin("*");
+		// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+		config.addAllowedHeader("*");
+		// #允许访问的头信息,*表示全部
+		config.addAllowedMethod("OPTIONS");
+		// 允许提交请求的方法类型，*表示全部允许
 		config.addAllowedMethod("HEAD");
 		config.addAllowedMethod("GET");
 		config.addAllowedMethod("PUT");
@@ -25,7 +30,8 @@ public class CorsWebFiler {
 		config.addAllowedMethod("DELETE");
 		config.addAllowedMethod("PATCH");
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);//拦截所有请求
+		source.registerCorsConfiguration("/**", config);
+		//拦截所有请求
 		return new CorsWebFilter(source);
 	}
 
